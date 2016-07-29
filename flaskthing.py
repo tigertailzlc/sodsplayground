75ec5153948872e1156083f3b2663a2580685590
tigertailzlc <tigertailzlc@gmail.com>
2016-07-29 16:19:34 +0800

    trying port issue fix

--- flaskthing.py
+++ flaskthing.py
@@ -7,5 +7,10 @@ def hello():
 
 if __name__ == "__main__":
     #app.run()
-    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0')
     #Be careful: http://flask.pocoo.org/docs/0.10/quickstart/ 
+    
+    #JK, trying this from http://stackoverflow.com/questions/17260338/deploying-flask-with-heroku: 
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
